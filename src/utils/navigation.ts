14ac57576d651db618009f1cde4bd73bc20585b9
jacky-alv96 <spanishwithjacky@gmail.com>
2025-01-13 19:11:42 -0600

    tweaks

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,41 +1,18 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "About", url: "/about" },
-  { name: "Services", url: "/services" },
+  // { name: "About", url: "/about" },
+  { name: "Projects", url: "/projects" },
   { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
-const footerLinks = [
-  {
-    section: "Ecosystem",
-    links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "About Section", url: "/about" },
-      { name: "Construction Services", url: "/services" },
-    ],
-  },
-  {
-    section: "Company",
-    links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
-    ],
-  },
-];
 // An object of links for social icons
 const socialLinks = {
   facebook: "https://www.facebook.com/",
-  x: "https://twitter.com/",
-  github: "https://github.com/mearashadowfax/ScrewFast",
   google: "https://www.google.com/",
-  slack: "https://slack.com/",
 };
 
 export default {
   navBarLinks,
-  footerLinks,
   socialLinks,
 };
